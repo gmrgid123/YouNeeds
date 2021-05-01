6bc1e700e0c61da65c6792673b63ca41d0964a2f
gmrgi <gmrgi@DESKTOP-TUVPUJ8>
2021-05-01 15:37:50 +0900

    # WARNING: head commit changed in the meantime
    확인

--- src/main/java/com/web/youneeds/controller/HomeController.java
+++ src/main/java/com/web/youneeds/controller/HomeController.java
@@ -1,21 +1,13 @@
 package com.web.youneeds.controller;
 
 import java.util.Locale;
-import java.util.Random;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
